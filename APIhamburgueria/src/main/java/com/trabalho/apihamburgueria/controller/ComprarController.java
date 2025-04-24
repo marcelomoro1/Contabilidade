f1cc5eadb7629e2da74f12d2dfffb219ad213474
Marcelo Moro <101741773+marcelomoro1@users.noreply.github.com>
2025-04-24 02:47:26 -0300

    Delete APIhamburgueria directory

--- APIhamburgueria/src/main/java/com/trabalho/apihamburgueria/controller/ComprarController.java
+++
@@ -1,4 +0,0 @@
-package com.trabalho.apihamburgueria.controller;
-
-public class ComprarController {
-}
--- APIhamburgueria/src/main/java/com/trabalho/apihamburgueria/repository/ProdutoRepository.java
+++
@@ -1,8 +0,0 @@
-package com.trabalho.apihamburgueria.repository;
-
-import com.trabalho.apihamburgueria.model.Produto;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ProdutoRepository extends JpaRepository<Produto, Long> {
-
-}
