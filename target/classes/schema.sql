b5a41019dcdf38f58237d19ca98db2ee011acebb
Marcelo <marcelo.p.moro@hotmail.com>
2025-07-09 18:47:58 -0300

    Atualização bug caixa

--- target/classes/schema.sql
+++
@@ -1,9 +0,0 @@
--- Verifica se a tabela existe e a cria se não existir
-CREATE TABLE IF NOT EXISTS clientes (
-    id BIGINT AUTO_INCREMENT PRIMARY KEY,
-    nome VARCHAR(100) NOT NULL,
-    cpf VARCHAR(14) UNIQUE NOT NULL,
-    cidade VARCHAR(100) NOT NULL,
-    estado VARCHAR(2) NOT NULL,
-    tipo_pessoa VARCHAR(10) NOT NULL
-); 
